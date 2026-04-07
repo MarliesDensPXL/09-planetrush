2c9f48ea6af7c3a30094d117af8e28a90f3cb59b
Marlies Dens <marlies.dens@pxl.be>
2026-04-07 22:00:02 +0200

    PLAYER-01

---
+++ PlanetRush/Models/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetRush.Models
+{
+    internal class Player
+    {
+		private int _tonsOfTrilliumAlloys;
+
+		public int TonsOfTrilliumAlloys
+		{
+			get { return _tonsOfTrilliumAlloys; }
+			set { _tonsOfTrilliumAlloys = value; }
+		}
+
+		private int _tonsOfRawAetherium;
+
+		public int TonsOfRawAetherium
+		{
+			get { return _tonsOfRawAetherium; }
+			set { _tonsOfRawAetherium = value; }
+		}
+
+		private Spacecraft _spacecraft;
+
+		public Spacecraft Spacecraft
+		{
+			get { return _spacecraft; }
+			set { _spacecraft = value; }
+		}
+
+	}
+}
