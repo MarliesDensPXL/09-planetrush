74e64cf770c58770c38d1ee560dfd505fbf38533
Marlies Dens <marlies.dens@pxl.be>
2026-04-12 15:53:25 +0200

    TRAVELLOG-02

--- PlanetRush/Models/TravelLog.cs
+++ PlanetRush/Models/TravelLog.cs
@@ -6,8 +6,45 @@ using System.Threading.Tasks;
 
 namespace PlanetRush.Models
 {
-    internal class TravelLog
+    public class TravelLog
     {
         private string[] _actionsTaken = { "Terraformed", "Genestealed", "Nuked" };
-    }
+
+		private int _number;
+
+		public int Number
+		{
+			get { return _number; }
+			set { _number = value; }
+		}
+
+		private Planet _planetVisited;
+
+		public Planet PlanetVisited
+		{
+			get { return _planetVisited; }
+			set { _planetVisited = value; }
+		}
+
+		private string _actionTaken;
+
+		public string ActionTaken
+		{
+			get { return _actionTaken; }
+			set { _actionTaken = value; }
+		}
+
+		private bool _isSuccesfullyDepleted;
+
+		public bool IsSuccesfullyDepleted
+		{
+			get { return _isSuccesfullyDepleted; }
+			set { _isSuccesfullyDepleted = value; }
+		}
+
+
+
+
+
+	}
 }
